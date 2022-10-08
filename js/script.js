4d3d8586b3522eeb08cd5e3395f5309df176fd90
Fares Mansour <78617685+FaresMansour970@users.noreply.github.com>
2022-10-08 15:21:51 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* =================== typing animation ================== */
+var typed = new Typed(".typing",{
+    starings:["Web Designer","Web Developer","Graphic Designer","YouTuber"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+}
